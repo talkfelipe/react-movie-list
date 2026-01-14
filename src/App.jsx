d2f317f4d48843a2151efe0343ca80d350db77d2
Felipe Silva <talk.felipe@gmail.com>
2026-01-14 15:45:02 +0900

    added the seach jsx hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useEffect, useState } from "react"
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked, title]);
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br /> {count}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <div className="card-container">
+    <main>
+      <div className="pattern"/>
 
-      <Card title="Star Wars" rating={5} isCool={true}/>
-      <Card title="Avatar"/>
-      <Card title="The Lion King"/>
-    </div>
-  )
-}
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
 
-export default App
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  );
+};
+
+export default App;
